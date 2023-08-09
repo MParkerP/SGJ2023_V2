7c4d4369d3e1bc05e4e97bc1ee7a721336535112
Parker Posey <mparkerposey@outlook.com>
2023-08-08 19:59:20 -0500

    added starting room that opens when both players are present

---
+++ Assets/Scripts/StartGameController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class StartGameController : NetworkBehaviour
+{
+    [SerializeField] private GameObject startingDoor;
+    [SerializeField] private float doorSpeed;
+
+    public void OpenDoor()
+    {
+        OpenDoorServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void OpenDoorServerRpc()
+    {
+        OpenDoorClientRpc();
+    }
+
+    [ClientRpc]
+    private void OpenDoorClientRpc()
+    {
+        StartCoroutine(slidingDoor());
+    }
+
+    IEnumerator slidingDoor()
+    {
+        startingDoor.GetComponent<Rigidbody2D>().velocity = Vector3.down.normalized * doorSpeed;
+        yield return new WaitForSeconds(10);
+        startingDoor.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+    }
+
+
+}
